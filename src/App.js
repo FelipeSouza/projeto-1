ee40d09fa072adefc68ab75bb4a7dff81b42098d
Felipe de Souza Oliveira Martins <felipeso@hagf.corp>
2021-07-07 10:35:25 -0300

    state com array de objetos

--- src/App.js
+++ src/App.js
@@ -1,88 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-//Component de Classe
-//Component Stateless
 class App extends Component{
-  //Toda vez que chamamos o constructor chamamos o props e o super
-  //constructor(props){
-   // super(props);
-    //bind method para reconhecer o  this dentro  do  método
-    //podemos remover o  bind se utilizarmos arrow function
-    //this.handlePClick = this.handlePClick.bind(this);
     state={
-    //this.state={
-      name: 'Felipe Martins',
-      counter:0
+      posts: [
+        {
+          id: 1,
+          title: 'O titulo 1',
+          body: 'O corpo 1'
+        },
+        {
+          id: 2,
+          title: 'O titulo 2',
+          body: 'O corpo 2'
+        },
+        {
+          id: 3,
+          title: 'O titulo 3',
+          body: 'O corpo 3'
+        }
+      ]
+    
     };
 
-  handlePClick=()=>{
-    
-   this.setState({name: 'Patrícia'});
-  }
+    render(){
+      const { posts } = this.state;
 
-  /*Usando arrow function não precisa do this
-    logo ele vai buscar/ herdar o  this da classe pai, então 
-    teremos o proprio this*/
-  handleAClick=(event)=>{
-    //para o  evento principal
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter + 1});
-    
-   }
-
-  render(){
-    //sem destructuring
-    //const name = this.state.name;
-
-    //Com destructuring
-    const { name, counter } = this.state;
-    return(
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={ this.handlePClick /*()=>console.log('<p> clicado')*/}>
-          { name } {counter}
-        </p>
-        <a onClick={ this.handleAClick}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Este é o link
-        </a>
-      </header>
-    </div>
-    );
-  }
+      return(
+        <div className="App">
+          { posts.map(post=> (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      );
+    }
 }
-
-
-
-//Component de Função
-//component statless
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 export default App;
